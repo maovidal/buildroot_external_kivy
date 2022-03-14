bb4cdf992a1f882819316a531169cae21d5f49fb
Mauricio Vidal <MrMauro@users.noreply.github.com>
2022-03-14 10:27:56 -0500

    Kivy package reorganized

--- package/python-kivy/python-kivy.mk
+++ package/python-kivy/python-kivy.mk
@@ -1,9 +1,6 @@
 ################################################################################
 #
 ## kivy
-## https://forums.raspberrypi.com/viewtopic.php?t=307052
-## https://forums.raspberrypi.com/viewtopic.php?t=242588
-## http://lists.busybox.net/pipermail/buildroot/2019-June/253526.html
 #
 #################################################################################
 
@@ -14,57 +11,57 @@ PYTHON_KIVY_LICENSE = MIT
 PYTHON_KIVY_LICENSE_FILES = LICENSE
 
 PYTHON_KIVY_DEPENDENCIES = host-python-cython
+PYTHON_KIVY_DEPENDENCIES += sdl2 sdl2_image sdl2_mixer sdl2_ttf  # From oficial documentation.
+PYTHON_KIVY_DEPENDENCIES += mtdev python-docutils python-pygments  # From oficial documentation.
+PYTHON_KIVY_DEPENDENCIES += util-linux pango  # Otherwise build fails with some unidentified cases.
 
-# From: https://kivy.org/doc/stable/api-kivy.graphics.cgl.html
-# Unix available GL backends: gl, sdl2
+# Regarding compilation options.
+# Inspecting build, those are avilable in Kivy 2.1.0
+#  * use_rpi
+#  * use_egl
+#  * use_opengl_es2
+#  * use_opengl_mock
+#  * use_sdl2
+#  * use_pangoft2
+#  * use_ios
+#  * use_android
+#  * use_mesagl
+#  * use_x11
+#  * use_wayland
+#  * use_gstreamer
+#  * use_avfoundation
+#  * use_osx_frameworks
+#  * debug_gl
+#  * kivy_sdl_gl_alpha_size
+#  * debug
+# Remark: USE_SDL seems to be deprecated in Kivy 2.1.0?
 
-# TESTING: Intel with GL
+PYTHON_KIVY_ENV += USE_SDL2=1
+PYTHON_KIVY_ENV += KIVY_SDL2_PATH=$(STAGING_DIR)/usr/include/SDL2  # Without this, build reports ERROR: unsafe header/library path used in cross-compilation: '-I/usr/local/include/SDL2'
+
+#
+## OpenGL or OpenGLES
+#
+
 ifeq ($(BR2_PACKAGE_HAS_LIBGL),y)
     PYTHON_KIVY_DEPENDENCIES += libgl
-    # PYTHON_KIVY_ENV += USE_OPENGL_ES2=1
-    # From https://kivy.org/doc/stable/api-kivy.graphics.cgl.html
-    PYTHON_KIVY_ENV += USE_OPENGL_MOCK=1
-endif
+    # PYTHON_KIVY_ENV += USE_OPENGL_MOCK=1  # TODO: Should it be here? disable linking to libGL
 
-ifeq ($(BR2_PACKAGE_HAS_LIBGLES),y)
+else ifeq ($(BR2_PACKAGE_HAS_LIBGLES),y)
     PYTHON_KIVY_DEPENDENCIES += libgles
     PYTHON_KIVY_ENV += USE_OPENGL_ES2=1
-    # disable linking to libGL
-    PYTHON_KIVY_ENV += USE_OPENGL_MOCK=1
+    # PYTHON_KIVY_ENV += USE_OPENGL_MOCK=1  # disable linking to libGL
 endif
+
+#
+## Window managers
+#
 
 ifeq ($(BR2_PACKAGE_HAS_LIBEGL),y)
     PYTHON_KIVY_DEPENDENCIES += libegl
     PYTHON_KIVY_ENV += USE_EGL=1
-    # From https://kivy.org/doc/stable/api-kivy.graphics.cgl.html
-    PYTHON_KIVY_ENV += USE_OPENGL_MOCK=1
 else
     PYTHON_KIVY_ENV += USE_EGL=0
-endif
-
-# From BR2_PACKAGE_GSTREAMER to BR2_PACKAGE_GSTREAMER1
-# ifeq ($(BR2_PACKAGE_GSTREAMER1),y)
-ifeq ($(BR2_PACKAGE_GSTREAMER),y)
-    PYTHON_KIVY_DEPENDENCIES += gstreamer1
-    PYTHON_KIVY_ENV += USE_GSTREAMER=1
-else
-    PYTHON_KIVY_ENV += USE_GSTREAMER=0
-endif
-
-ifeq ($(BR2_PACKAGE_SDL2)$(BR2_PACKAGE_SDL2_IMAGE)$(BR2_PACKAGE_SDL2_MIXER)$(BR2_PACKAGE_SDL2_TTF),yyyy)
-    PYTHON_KIVY_DEPENDENCIES += sdl2 sdl2_image sdl2_mixer sdl2_ttf
-    PYTHON_KIVY_ENV += USE_SDL2=1
-    # Without this there is an ERROR: unsafe header/library path used in cross-compilation: '-I/usr/local/include/SDL2'
-    PYTHON_KIVY_ENV += KIVY_SDL2_PATH=$(STAGING_DIR)/usr/include/SDL2
-else
-    PYTHON_KIVY_ENV += USE_SDL2=0
-endif
-
-ifeq ($(BR2_PACKAGE_WAYLAND),y)
-    PYTHON_KIVY_DEPENDENCIES += wayland
-    PYTHON_KIVY_ENV += USE_WAYLAND=1
-else
-    PYTHON_KIVY_ENV += USE_WAYLAND=0
 endif
 
 ifeq ($(BR2_PACKAGE_XLIB_LIBX11)$(BR2_PACKAGE_XLIB_LIBXRENDER),yy)
@@ -74,9 +71,34 @@ else
     PYTHON_KIVY_ENV += USE_X11=0
 endif
 
-# Raspberry Pi exclusive (At least for Pi2)
-# TODO: Find a better way to determine a Raspberry Pi
-ifeq ($(BR2_arm)$(BR2_cortex_a7)$(BR2_ARM_FPU_NEON_VFPV4),yyy)
+#
+## Other configuration options for the build
+#
+
+# # TODO: Confirm if this is correct for USE_MESAGL introduced in Kivy 2.1.0
+# ifeq ($(BR2_PACKAGE_MESA3D),y)
+#     PYTHON_KIVY_DEPENDENCIES += mesa3d
+#     PYTHON_KIVY_ENV += USE_MESAGL=1
+# else
+#     PYTHON_KIVY_ENV += USE_MESAGL=0
+# endif
+
+ifeq ($(BR2_PACKAGE_WAYLAND),y)
+    PYTHON_KIVY_DEPENDENCIES += wayland
+    PYTHON_KIVY_ENV += USE_WAYLAND=1
+else
+    PYTHON_KIVY_ENV += USE_WAYLAND=0
+endif
+
+ifeq ($(BR2_PACKAGE_PYTHON_KIVY_GSTREAMER),y)
+    PYTHON_KIVY_DEPENDENCIES += gstreamer1
+    PYTHON_KIVY_ENV += USE_GSTREAMER=1  # Should it be GSTREAMER (deprecated?) or GSTREAMER1
+else
+    PYTHON_KIVY_ENV += USE_GSTREAMER=0  # Should it be GSTREAMER (deprecated?) or GSTREAMER1
+endif
+
+# Raspberry Pi exclusive
+ifeq ($(BR2_PACKAGE_PYTHON_KIVY_RPI),y)
     PYTHON_KIVY_DEPENDENCIES += rpi-userland
     # The next two (2) are based on https://github.com/kivy/kivy/pull/5866
     PYTHON_KIVY_ENV += KIVY_CROSS_PLATFORM=rpi
@@ -84,29 +106,12 @@ ifeq ($(BR2_arm)$(BR2_cortex_a7)$(BR2_ARM_FPU_NEON_VFPV4),yyy)
 endif
 
 define PYTHON_KIVY_REMOVE_EXAMPLES
-    rm -rf $(TARGET_DIR)/usr/share/kivy-examples
+    ifeq ($(BR2_PACKAGE_PYTHON_KIVY_EXAMPLES),n)
+        rm -rf $(TARGET_DIR)/usr/share/kivy-examples
+        echo "Kivy examples removed"
+    endif
 endef
 
 PYTHON_KIVY_POST_INSTALL_TARGET_HOOKS += PYTHON_KIVY_REMOVE_EXAMPLES
 
 $(eval $(python-package))
-
-
-# Build configuration is:
-#  * use_rpi = 0
-#  * use_egl = 0 OK
-#  * use_opengl_es2 = 0 OK con duda
-#  * use_opengl_mock = 0 OK con duda
-#  * use_sdl2 = 1 OK
-#  * use_pangoft2 = 0
-#  * use_ios = 0
-#  * use_android = 0
-#  * use_mesagl = 0
-#  * use_x11 = 0 OK
-#  * use_wayland = 0 OK
-#  * use_gstreamer = 1 OK con duda
-#  * use_avfoundation = 0
-#  * use_osx_frameworks = 0
-#  * debug_gl = 0
-#  * kivy_sdl_gl_alpha_size = 8
-#  * debug = False

0d227dbce1c0f14c4ba4f3f86767dcb33d52a7bd
Mauricio Vidal <MrMauro@users.noreply.github.com>
2022-03-14 10:32:01 -0500

    Test reorganization

---
+++ rootfs_overlay/kivyapp/probe_mesa_gl.py
@@ -0,0 +1,13 @@
+import os
+# tell kivy a couple of home truths
+os.environ['KIVY_GL_BACKEND']='mesagl'
+os.environ['HOME']='/root'
+
+from kivy.app import App
+from kivy.uix.button import Button
+
+class TestApp(App):
+    def build(self):
+        return Button(text='Hello World')
+
+TestApp().run()
---
+++ rootfs_overlay/kivyapp/simpletest.py
@@ -0,0 +1,10 @@
+import os
+
+from kivy.app import App
+from kivy.uix.button import Button
+
+class TestApp(App):
+    def build(self):
+        return Button(text='Hello World')
+
+TestApp().run()
